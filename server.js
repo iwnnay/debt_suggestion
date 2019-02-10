6130d846200a3022b0ebf86ab87dacd7190e80f5
Joe Imhoff <iwnnay@gmail.com>
2019-02-10 20:06:43 +0000

    Heroku is failing when trying to listen
    Conflicts:
    	server.js

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const express = require('express')
 
 exports.start = () => {
   const PORT = process.env.PORT || 5000;
-  const HOST = process.env.HOST || undefined;
+  const HOST = process.env.HOST || false;
 
-  express()
-    .use(express.static(path.join(__dirname, 'public')))
+  const app = express();
+
+  app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, HOST, () => console.log(`Listening on ${ PORT }`));
+
+  if (HOST) {
+    app.listen(PORT, HOST, () => console.log(`Listening on ${ PORT }`));
+  } else {
+    app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  }
 };
 
