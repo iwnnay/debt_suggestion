38bcdeb68d8d771621012402c944d4daeced3128
Joe Imhoff <iwnnay@gmail.com>
2019-02-10 19:55:46 +0000

    Default to no defined HOST for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 exports.start = () => {
   const PORT = process.env.PORT || 5000;
-  const HOST = process.env.HOST || '127.0.0.1';
+  const HOST = process.env.HOST || undefined;
 
   express()
     .use(express.static(path.join(__dirname, 'public')))
