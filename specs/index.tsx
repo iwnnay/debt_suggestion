ee904f32d13edcbce1eaa43fd37facc411b3583c
Joe Imhoff <iwnnay@gmail.com>
2019-02-26 04:03:35 +0000

    Stable with jest

--- specs/index.tsx
+++ specs/index.tsx
@@ -1,2 +1,11 @@
+import * as React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import Calculator from '../src/components/calculator';
+
 describe('main app file', () => {
+  it('should be able to render a calculator', () => {
+    const app = TestRenderer.create(<Calculator />).root;
+
+    expect(app.findByType('main')).not.toBeNull();
+  });
 });
